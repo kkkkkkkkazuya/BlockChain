1032fa9705d47d95a57d26caadcc0e078e509b2e
kkkkkkkkazuya <kkazuya.baseball0508@gmail.com>
2023-01-07 22:35:37 +0900

    add Block Struct

--- goblockchain.go
+++ goblockchain.go
@@ -3,13 +3,42 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 )
+
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions []string
+}
+
+/**
+* nonce int ナンス
+* previousHash string
+* return block
+**/
+
+func NewBlock(nonce int, previousHash string) *Block {
+
+	block := new(Block)
+	block.timestamp = time.Now().UnixNano()
+	block.nonce = nonce
+	block.previousHash = previousHash
+	return block
+
+	/*
+		return &Block{
+			timestamp: time.Now().UnixMicro(),
+		}
+	*/
+}
 
 func init() {
 	log.SetPrefix("BlockChain: ")
 }
 
 func main() {
-	log.Println("Test1")
-	fmt.Println("Test2")
+	block := NewBlock(0, "init Hash")
+	fmt.Println(block)
 }
